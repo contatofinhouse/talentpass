acc5c0731d7d309d25862ebd719504f80f202633
contatofinhouse <contatofinhouse@gmail.com>
2025-10-28 11:58:52 -0300

    vercel create speedinsight

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,58 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
-import Signup from "./pages/Signup";
-import Auth from "./pages/Auth";
-import Welcome from "./pages/Welcome";
-import ManagerDashboard from "./pages/ManagerDashboard";
-import EmployeeDashboard from "./pages/EmployeeDashboard";
-import EmployeeLogin from "./pages/EmployeeLogin";
-import ResetPassword from "./pages/resetpassword";
-import PublicCourse from "./pages/PublicCourse";
-import SignupEmployee from "./pages/SignupEmployee";
+  import { Toaster } from "@/components/ui/toaster";
+  import { Toaster as Sonner } from "@/components/ui/sonner";
+  import { TooltipProvider } from "@/components/ui/tooltip";
+  import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+  import { BrowserRouter, Routes, Route } from "react-router-dom";
+  import Index from "./pages/Index";
+  import NotFound from "./pages/NotFound";
+  import Signup from "./pages/Signup";
+  import Auth from "./pages/Auth";
+  import Welcome from "./pages/Welcome";
+  import ManagerDashboard from "./pages/ManagerDashboard";
+  import EmployeeDashboard from "./pages/EmployeeDashboard";
+  import EmployeeLogin from "./pages/EmployeeLogin";
+  import ResetPassword from "./pages/ResetPassword";
+  import PublicCourse from "./pages/PublicCourse";
+  import SignupEmployee from "./pages/SignupEmployee";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+  import ProtectedRoute from "./components/ProtectedRoute";
+  import TermsConditions from "./pages/TermsConditions";
+  import PrivacyPolicy from "./pages/PrivacyPolicy";
+  import CorporateEducationGuide from "./pages/CorporateEducationGuide";
+  import { ScrollToTop } from "@/components/ScrollToTop";
 
-import ProtectedRoute from "./components/ProtectedRoute";
-import TermsConditions from "./pages/TermsConditions";
-import PrivacyPolicy from "./pages/PrivacyPolicy";
-import CorporateEducationGuide from "./pages/CorporateEducationGuide";
-import { ScrollToTop } from "@/components/ScrollToTop";
+  const queryClient = new QueryClient();
 
-const queryClient = new QueryClient();
+  const App = () => (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+        <ScrollToTop />
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Auth />} />
+            <Route path="/welcome" element={<ProtectedRoute><Welcome /></ProtectedRoute>} />
+            <Route path="/manager/dashboard" element={<ProtectedRoute role="manager"><ManagerDashboard /></ProtectedRoute>} />
+            <Route path="/dashboard" element={<ProtectedRoute role="employee"><EmployeeDashboard /></ProtectedRoute>} />
+            <Route path="/resetpassword" element={<ResetPassword />} />
+            
+            <Route path="/termos-e-condicoes" element={<TermsConditions />} />
+            <Route path="/politica-de-privacidade" element={<PrivacyPolicy />} />
+            <Route path="/guia-educacao-corporativa" element={<CorporateEducationGuide />} />
+            <Route path="/signupemployee" element={<SignupEmployee />} />
+            <Route path="/curso/:id" element={<PublicCourse />} />
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-      <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Auth />} />
-          <Route path="/welcome" element={<ProtectedRoute><Welcome /></ProtectedRoute>} />
-          <Route path="/manager/dashboard" element={<ProtectedRoute role="manager"><ManagerDashboard /></ProtectedRoute>} />
-          <Route path="/dashboard" element={<ProtectedRoute role="employee"><EmployeeDashboard /></ProtectedRoute>} />
-          <Route path="/resetpassword" element={<ResetPassword />} />
-          
-          <Route path="/termos-e-condicoes" element={<TermsConditions />} />
-          <Route path="/politica-de-privacidade" element={<PrivacyPolicy />} />
-          <Route path="/guia-educacao-corporativa" element={<CorporateEducationGuide />} />
-          <Route path="/signupemployee" element={<SignupEmployee />} />
-          <Route path="/curso/:id" element={<PublicCourse />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+          <SpeedInsights />
+      </TooltipProvider>
+    </QueryClientProvider>
+    
+  );
 
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-
-export default App;
+  export default App;
